db85229f5165441e9730b10255925ba1029dbcbf
s1053021 <e96031001@gmail.com>
2019-05-25 15:02:15 +0800

    refrctorcode

--- duplicate_code/Statistics.java
+++ duplicate_code/Statistics.java
@@ -4,38 +4,31 @@ public class Statistics
 {
     public double calculateDifferenceOfAverage(double[] array1, double[] array2)
     {
-        double sum1 = 0;
-        double average1;
-
-        for (double element : array1) {
-            sum1 += element;
-        }
-        average1 = sum1 / array1.length;
-
-        double sum2 = 0;
-        double average2;
-
-        for (double element : array2) {
-            sum2 += element;
-        }
-        average2 = sum2 / array2.length;
+        average1 = avg(array1)
+		average2 = avg(array2)
 
         return Math.abs(average1 - average2);
     }
 
     public double calculateSampleVariance(double[] elements)
     {
-        double sum1 = 0;
-        double average1;
-        for (double element : elements) {
-            sum1 += element;
-        }
-        average1 = sum1 / elements.length;
+        average1 = avg(elements)
         double temp = 0.0;
         for (double element : elements) {
             temp += Math.pow(element - average1, 2);
         }
         return temp / (elements.length - 1);
+	public double avg(double[] array)
+	{
+		double sum = 0;
+        double average;
 
+        for (double element : array) {
+            sum += element;
+        }
+        average = sum / array.length;
+		return average
+		
+	}
     }
 }
